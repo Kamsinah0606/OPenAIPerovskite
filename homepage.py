248e248259a384500f482d4280cfb249d26ff0f7
NOORHAFIZAH BINTI MUHAMMAD <161951791+Kamsinah0606@users.noreply.github.com>
2025-10-24 03:40:30 +0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -12,6 +12,9 @@ st.set_page_config(
 
 st.header("OPenAIPerovskite", divider="gray")
 
+# Define URL of dataset
+URL "https://raw.githubusercontent.com/Kamsinah0606/OPenAIPerovskite/refs/heads/research/Dataset%202D%20Perovskite%20(2016-2025)%20-%20Mixed.csv"
+
 # --- 2. DATA LOADING AND CLEANING ---
 @st.cache_data
 def load_data():
